be8f51d3b509f5b7067c096c839199d19d818135
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-05-09 20:23:07 +0200

    feat: add window creation and main loop to launcher

--- src/launcher/src/main.cpp
+++ src/launcher/src/main.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+// glad must be included before any other GL/GLFW headers.
+#include <glad/glad.h>
+
+#include "window.hpp"
 
 int main()
 {
-    std::cout << "Hello World from Launcher" << std::endl;
+    sisyphos::Window window{1280, 720, "Sisyphos"};
+
+    while (!window.shouldClose())
+    {
+        window.pollEvents();
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        window.swapBuffers();
+    }
+
     return 0;
 }
